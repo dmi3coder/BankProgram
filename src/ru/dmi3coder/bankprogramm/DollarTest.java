e47cbcdb5b59d77f0be38d3dfc5933cd1e50df1e
dmi3coder <dmi3coder@gmail.com>
2016-02-29 17:02:41 +0200

    Removed dollar and frank from classes(Simple coupling)

--- src/ru/dmi3coder/bankprogramm/DollarTest.java
+++ src/ru/dmi3coder/bankprogramm/DollarTest.java
@@ -23,7 +23,7 @@ public class DollarTest {
 
     @Test
     public void testFrankMultiplication(){
-        Franc five = Money.franc(5);
+        Money five = Money.franc(5);
         assertEquals(Money.franc(10),five.times(2));
         assertEquals(Money.franc(15),five.times(3));
     }
@@ -32,8 +32,6 @@ public class DollarTest {
     public void testEquality(){
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.dollar(5).equals(Money.franc(6)));
     }
 
@@ -43,11 +41,6 @@ public class DollarTest {
         assertEquals("CHF",Money.franc(1).currency());
     }
 
-    @Test
-    public void  testDifferentClassEquality(){
-        assertTrue(new Money(10,"CHF").equals(new Franc(10,"CHF")));
-
-    }
 
 
 }
